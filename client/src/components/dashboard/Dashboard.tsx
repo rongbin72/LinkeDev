f8ce64d2ddefd9043223ee36118ea3c146b6bc68
Robin <bin.googol@gmail.com>
2019-05-05 15:03:21 -0400

    dashboard

--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import { DashboardProps, StoreState } from '../../../common/types'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
+import { DashboardProps, StoreState } from '../../../common/types'
+import { getCurrentProfile } from '../../actions/profile'
 
-const Dashboard: React.FC<DashboardProps> = () => {
+const Dashboard: React.FC<DashboardProps> = ({ getCurrentProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfile()
+  }, [])
   return <>Dashboard</>
 }
 
-const mapStateToProps = (state: StoreState) => ({})
+const mapStateToProps = (state: StoreState) => ({
+  auth: state.auth,
+  profile: state.profile!.profile
+})
 
-export default connect(mapStateToProps)(Dashboard)
+export default connect(
+  mapStateToProps,
+  { getCurrentProfile }
+)(Dashboard)
