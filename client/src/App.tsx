14a43d3bca5024ce01d3364223b1a14dd5004c48
Robin <bin.googol@gmail.com>
2019-05-05 16:39:27 -0400

    Add createProfile route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,6 +13,7 @@ import setAuthToken from './utils/setAuthToken'
 import { loadUser } from './actions/auth'
 import Dashboard from './components/dashboard/Dashboard'
 import PrivateRoute from './components/routing/PrivateRoute'
+import CreateProfile from './components/profile_forms/CreateProfile'
 
 if (localStorage.token) setAuthToken(localStorage.token)
 
@@ -33,6 +34,7 @@ const App: React.FC = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute exact path='/create_profile' component={CreateProfile} />
             </Switch>
           </section>
         </>
