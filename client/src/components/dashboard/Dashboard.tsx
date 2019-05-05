f69b82ae12633320a4a74c31d1dc0c36e7c76ff1
Robin <bin.googol@gmail.com>
2019-05-05 16:06:25 -0400

    Add spinner and init profile page

--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -2,17 +2,40 @@ import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { DashboardProps, StoreState } from '../../../common/types'
 import { getCurrentProfile } from '../../actions/profile'
+import Spinner from '../layout/Spinner'
+import { Link } from 'react-router-dom'
 
-const Dashboard: React.FC<DashboardProps> = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard: React.FC<DashboardProps> = ({ getCurrentProfile, user, profile, loading }) => {
   useEffect(() => {
     getCurrentProfile()
-  }, [])
-  return <>Dashboard</>
+  }, [getCurrentProfile])
+  return loading && !profile ? (
+    <Spinner />
+  ) : (
+    <>
+      <h1 className='large text-primary'>Dashboard</h1>
+      <p className='lead'>
+        {' '}
+        <i className='fas fa-user' /> Welcome {user && user.name}{' '}
+      </p>
+      {profile ? (
+        <>has</>
+      ) : (
+        <>
+          <p>You have not yet setup profile, please add some info</p>
+          <Link to='/create-profile' className='btn btn-primary my-1'>
+            Create Profile
+          </Link>
+        </>
+      )}
+    </>
+  )
 }
 
 const mapStateToProps = (state: StoreState) => ({
-  auth: state.auth,
-  profile: state.profile!.profile
+  user: state.auth!.user!,
+  profile: state.profile!.profile!,
+  loading: state.profile!.loading
 })
 
 export default connect(
