1b63714a95429a8a0541d37b66339aa8f10d614e
Robin <bin.googol@gmail.com>
2019-05-06 13:30:20 -0400

    Education component

---
+++ client/src/components/dashboard/Education.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { EducationProps, StoreState } from '../../../common/types'
+import Moment from 'react-moment'
+
+const Education: React.FC<EducationProps> = ({ education }) => {
+  const educations = !education ? (
+    <></>
+  ) : (
+    education.map(edu => (
+      <tr key={edu._id}>
+        <td>{edu.school}</td>
+        <td className='hide-sm'>{edu.degree}</td>
+        <td>
+          <Moment format='YYYY/MM/DD'>{edu.from}</Moment> -{' '}
+          {edu.to ? <Moment format='YYYY/MM/DD'>{edu.to}</Moment> : 'Now'}
+        </td>
+        <td>
+          <button className='btn btn-danger'>Delete</button>
+        </td>
+      </tr>
+    ))
+  )
+
+  return (
+    <>
+      <h2 className='my-2'>Education Credentials</h2>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>School</th>
+            <th className='hide-sm'>Degree</th>
+            <th className='hide-sm'>Years</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>{educations}</tbody>
+      </table>
+    </>
+  )
+}
+
+export default Education
