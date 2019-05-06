6aaab7ed739ecae7e1186e79b6b6188ae563f750
Robin <bin.googol@gmail.com>
2019-05-06 17:20:30 -0400

    Add Profiles link

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -8,6 +8,9 @@ const Navbar: React.FC<NavbarProps> = ({ isAuth, loading, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to='/profiles'>Developers</Link>
+      </li>
+      <li>
         <Link to='/dashboard'>
           <i className='fas fa-user' /> <span className='hide-sm'>Dashboard</span>
         </Link>
@@ -23,7 +26,8 @@ const Navbar: React.FC<NavbarProps> = ({ isAuth, loading, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <a href='#!'>Developers</a>
+        {' '}
+        <Link to='/profiles'>Developers</Link>{' '}
       </li>
       <li>
         <Link to='/register'>Register</Link>
