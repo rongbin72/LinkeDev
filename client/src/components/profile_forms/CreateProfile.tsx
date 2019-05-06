a6b2cdb985a0e3a2d3b844d1a7a97d6a389a628c
Robin <bin.googol@gmail.com>
2019-05-05 23:00:51 -0400

    Fix Go back link

--- client/src/components/profile_forms/CreateProfile.tsx
+++ client/src/components/profile_forms/CreateProfile.tsx
@@ -3,6 +3,7 @@ import { CreateProfileProps, StoreState, ProfileForm } from '../../../common/typ
 import { connect } from 'react-redux'
 import { createProfile } from '../../actions/profile'
 import { withRouter } from 'react-router'
+import { Link } from 'react-router-dom'
 
 const CreateProfile: React.FC<CreateProfileProps> = ({ createProfile, history }) => {
   const [formData, setFormData] = useState<ProfileForm>({
@@ -217,9 +218,9 @@ const CreateProfile: React.FC<CreateProfileProps> = ({ createProfile, history })
         )}
 
         <input type='submit' className='btn btn-primary my-1' />
-        <a className='btn btn-light my-1' href='dashboard.html'>
+        <Link className='btn btn-light my-1' to='/dashboard'>
           Go Back
-        </a>
+        </Link>
       </form>
     </>
   )
