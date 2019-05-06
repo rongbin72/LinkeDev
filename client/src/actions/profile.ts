e5e6040a85d32414bf9c51b7ac664717bea75490
Robin <bin.googol@gmail.com>
2019-05-06 00:29:08 -0400

    Add action addExperience & addEducation

--- client/src/actions/profile.ts
+++ client/src/actions/profile.ts
@@ -1,9 +1,11 @@
 import axios from 'axios'
 import {
+  AddExperienceAction,
   CreateProfileAction,
   ErrorRes,
   GetCurrentProfileAction,
-  ProfileType
+  ProfileType,
+  AddEducationAction
 } from '../../common/types'
 import { setAlert } from './alert'
 import { ProfileStatus } from './types'
@@ -45,6 +47,61 @@ export const createProfile: CreateProfileAction = (
     dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success '))
 
     if (!edit) history.push('/dashboard')
+  } catch (error) {
+    const errors: ErrorRes[] = error.response.data.errors
+    if (errors) errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+
+    dispatch({
+      type: ProfileStatus.PROFILE_ERROR,
+      payload: {
+        error: {
+          msg: error.response.data.msg as string,
+          status: error.response.status as number
+        }
+      }
+    })
+  }
+}
+
+// Add Experience
+export const addExperience: AddExperienceAction = (formData, history) => async dispatch => {
+  try {
+    const res = await axios.put<ProfileType>('/api/profiles/experience', formData)
+    dispatch({
+      type: ProfileStatus.UPDATE_PROFILE,
+      payload: { profile: res.data }
+    })
+
+    dispatch(setAlert('Experience Added', 'success '))
+    history.push('/dashboard')
+  } catch (error) {
+    console.error(error)
+    const errors: ErrorRes[] = error.response.data.errors
+    if (errors) errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+
+    dispatch({
+      type: ProfileStatus.PROFILE_ERROR,
+      payload: {
+        error: {
+          msg: error.response.data.msg as string,
+          status: error.response.status as number
+        }
+      }
+    })
+  }
+}
+
+// Add Education
+export const addEducation: AddEducationAction = (formData, history) => async dispatch => {
+  try {
+    const res = await axios.put<ProfileType>('/api/profiles/education', formData)
+    dispatch({
+      type: ProfileStatus.UPDATE_PROFILE,
+      payload: { profile: res.data }
+    })
+
+    dispatch(setAlert('Education Added', 'success '))
+    history.push('/dashboard')
   } catch (error) {
     const errors: ErrorRes[] = error.response.data.errors
     if (errors) errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
