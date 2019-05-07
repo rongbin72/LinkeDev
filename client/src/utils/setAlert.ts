6b5875528c9ec8247fea3978d1e1b2b7b3039a53
Robin <bin.googol@gmail.com>
2019-05-07 13:02:39 -0400

    Handle alert stype

--- client/src/utils/setAlert.ts
+++ client/src/utils/setAlert.ts
@@ -1,5 +1,21 @@
-import { ToastType, toast, Toast } from 'react-toastify'
+import { toast, Slide, Flip, Zoom } from 'react-toastify'
 
 export default function(msg: string, type: string) {
-  toast.success(msg)
+  switch (type) {
+    case toast.TYPE.SUCCESS:
+      toast.success(msg, {
+        autoClose: 1500,
+        position: toast.POSITION.BOTTOM_RIGHT,
+        transition: Slide
+      })
+      break
+    case toast.TYPE.ERROR:
+      toast.error(msg, { autoClose: 2000, position: toast.POSITION.TOP_CENTER })
+      break
+    case toast.TYPE.INFO:
+      toast.error(msg, { autoClose: 2000, position: toast.POSITION.BOTTOM_RIGHT })
+      break
+    default:
+      toast(msg)
+  }
 }
