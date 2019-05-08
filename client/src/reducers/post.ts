79b59daff34a91f6d35906cfb105e99063a9bf20
Robin <bin.googol@gmail.com>
2019-05-07 22:58:50 -0400

    Complete post reducer

--- client/src/reducers/post.ts
+++ client/src/reducers/post.ts
@@ -1,4 +1,12 @@
-import { PostPayload, PostState, TAction } from '../../common/types'
+import {
+  PostPayload,
+  PostState,
+  PostType,
+  TAction,
+  TError,
+  UpdateLikes,
+  CommentType
+} from '../../common/types'
 import { PostStatus } from '../actions/types'
 
 const initialState: PostState = {
@@ -11,16 +19,69 @@ const initialState: PostState = {
 export default function(state = initialState, action: TAction<PostStatus, PostPayload>): PostState {
   const { type, payload } = action
   switch (type) {
+    case PostStatus.GET_POST:
+      return {
+        ...state,
+        post: payload as PostType,
+        loading: false
+      }
     case PostStatus.GET_POSTS:
       return {
         ...state,
-        posts: payload.posts || [],
+        posts: payload as PostType[],
         loading: false
       }
+    case PostStatus.ADD_POST:
+      return {
+        ...state,
+        posts: [payload as PostType, ...state.posts],
+        loading: false
+      }
+    case PostStatus.DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(post => post._id !== (payload as string)),
+        loading: false
+      }
+    case PostStatus.UPDATE_LIKES:
+      return {
+        ...state,
+        posts: state.posts.map(post =>
+          post._id === (payload as UpdateLikes).post_id
+            ? { ...post, likes: (payload as UpdateLikes).likes }
+            : post
+        ),
+        loading: false
+      }
+    case PostStatus.ADD_COMMENT:
+      return {
+        ...state,
+        post: { ...state.post!, comments: payload as CommentType[] },
+        loading: false
+      }
+
+    case PostStatus.DELETE_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post!,
+          comments: payload as CommentType[]
+        },
+        loading: false
+      }
+
+    case PostStatus.CLEAR_POST:
+      return {
+        ...state,
+        post: null,
+        loading: true
+      }
+
     case PostStatus.POST_ERROR:
       return {
         ...state,
-        error: payload.error || null
+        error: payload as TError,
+        loading: false
       }
     default:
       return state
