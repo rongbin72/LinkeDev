513138ede34082329d0d312c7756faf867a9b682
Robin <bin.googol@gmail.com>
2019-05-07 23:07:55 -0400

    Prepare to depoly

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import users from './routes/api/users'
 import auth from './routes/api/auth'
 import profiles from './routes/api/profiles'
 import posts from './routes/api/posts'
+import path from 'path'
 
 const app = express()
 
@@ -12,14 +13,20 @@ connectDB()
 //init Middleware
 app.use(express.json())
 
-app.get('/', (_, res) => res.send('API Running'))
-
 // Define Routes
 app.use('/api/users', users)
 app.use('/api/auth', auth)
 app.use('/api/profiles', profiles)
 app.use('/api/posts', posts)
 
+// Serve static asset for production
+if (process.env.NODE_ENV === 'production') {
+  // Set static path
+  app.use(express.static('client/build'))
+  app.get('*', (_, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server Running on port ${PORT}`))
