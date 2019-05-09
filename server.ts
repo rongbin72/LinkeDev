a25a26a2a13106dcec8a68ab8bb2ed653d107a19
rongbin72 <bin.googol@gmail.com>
2019-05-08 22:52:24 -0400

    Fix client route

--- server.ts
+++ server.ts
@@ -24,8 +24,7 @@ if (process.env.NODE_ENV === 'production') {
   // Set static path
   app.use(express.static('client/build'))
   app.get('*', (_, res) => {
-    res.sendFile('index.html')
-    // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
   })
 }
 const PORT = process.env.PORT || 5000
