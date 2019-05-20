54e11b4850ed7a796951ba0f4ab400d451b8bb18
Robin <bin.googol@gmail.com>
2019-05-20 16:44:45 -0400

    auth resolver

---
+++ routes/graphql/resolvers/auth.ts
@@ -0,0 +1,49 @@
+import axios from 'axios'
+import { Request } from 'express'
+
+const REST_ROOT = 'http://localhost:5000'
+export default {
+  login: async (_: any, loginData: { email: string; password: string }) => {
+    try {
+      const res = await axios.post<{ token: string }>(
+        `${REST_ROOT}/api/auth`,
+        loginData
+      )
+      return res.data
+    } catch (error) {
+      throw error
+    }
+  },
+
+  register: async (
+    _: any,
+    registerData: {
+      name: string
+      email: string
+      password: string
+    }
+  ) => {
+    try {
+      const res = await axios.post<{ token: string }>(
+        `${REST_ROOT}/api/auth`,
+        registerData
+      )
+      return res.data
+    } catch (error) {
+      throw error
+    }
+  },
+
+  user: async (_: any, __: any, context: { token: string }) => {
+    try {
+      const res = await axios.get(`${REST_ROOT}/api/auth`, {
+        headers: {
+          'x-auth-token': context.token
+        }
+      })
+      return res.data
+    } catch (error) {
+      throw error
+    }
+  }
+}
