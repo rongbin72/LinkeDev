e008229a9b8cf7db6cec5669a23a69b0c9e1f9b4
Robin <bin.googol@gmail.com>
2019-05-21 16:39:09 -0400

    Implemented and tested auth resolvers

--- routes/graphql/resolvers/auth.ts
+++ routes/graphql/resolvers/auth.ts
@@ -1,42 +1,61 @@
+import { AuthenticationError, UserInputError } from 'apollo-server-core'
 import axios from 'axios'
-import { Request } from 'express'
+import { IFieldResolver, IResolverObject } from 'graphql-tools'
+import { AuthData } from './index'
+
+interface LoginInput {
+  email: string
+  password: string
+}
+
+interface RegisterInput extends LoginInput {
+  name: string
+}
 
 const REST_ROOT = 'http://localhost:5000'
-export default {
-  login: async (loginData: { email: string; password: string }) => {
-    try {
-      const res = await axios.post(`${REST_ROOT}/api/auth`, loginData)
-      return res.data
-    } catch (error) {
-      const errors: { msg: string }[] = error.response.data.errors
-      return new Error(errors.map(e => e.msg).join('|'))
-    }
-  },
 
-  register: async (registerData: {
-    name: string
-    email: string
-    password: string
-  }) => {
-    try {
-      const res = await axios.post(`${REST_ROOT}/api/users`, registerData)
-      return res.data
-    } catch (error) {
-      const errors: { msg: string }[] = error.response.data.errors
-      return new Error(errors.map(e => e.msg).join('|'))
-    }
-  },
-
-  user: async (_: void, req: Request) => {
-    try {
-      const res = await axios.get(`${REST_ROOT}/api/auth`, {
-        headers: {
-          'x-auth-token': req.header('x-auth-token')
-        }
-      })
-      return res.data
-    } catch (error) {
-      return new Error(error)
-    }
+const login: IFieldResolver<any, AuthData, LoginInput> = async (
+  _,
+  loginInput
+) => {
+  try {
+    const res = await axios.post(`${REST_ROOT}/api/auth`, loginInput)
+    return res.data
+  } catch (error) {
+    const errors: { msg: string }[] = error.response.data.errors
+    return new UserInputError('Authentication Error', { details: errors })
   }
 }
+
+const register: IFieldResolver<any, AuthData, RegisterInput> = async (
+  _,
+  registerInput
+) => {
+  try {
+    const res = await axios.post(`${REST_ROOT}/api/users`, registerInput)
+    return res.data
+  } catch (error) {
+    const errors: { msg: string }[] = error.response.data.errors
+    return new UserInputError('Invalid Inputs', { details: errors })
+  }
+}
+
+const user: IFieldResolver<any, AuthData, null> = async (_, __, auth) => {
+  try {
+    const res = await axios.get(`${REST_ROOT}/api/auth`, {
+      headers: {
+        'x-auth-token': auth.token
+      }
+    })
+    return res.data
+  } catch (error) {
+    return new AuthenticationError('Unauthenticated Access')
+  }
+}
+
+const authResolver: { [key: string]: IResolverObject<any, AuthData> } = {
+  Query: { login, user },
+  Mutation: { register }
+}
+
+export default authResolver
