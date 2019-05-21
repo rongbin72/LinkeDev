5363f55fad5d4142e2633fbc46b7ded225ab5efc
Robin <bin.googol@gmail.com>
2019-05-21 19:47:01 -0400

    Root resolves

--- routes/graphql/resolvers/index.ts
+++ routes/graphql/resolvers/index.ts
@@ -1,9 +1,13 @@
+import auth from './auth'
 import posts from './posts'
 import profiles from './profiles'
-import auth from './auth'
-
-export default {
-  ...posts,
-  ...profiles,
-  ...auth
+export interface AuthData {
+  token: string
 }
+
+const resolvers = {
+  Query: { ...auth.Query, ...posts.Query, ...profiles.Query },
+  Mutation: { ...auth.Mutation, ...posts.Mutation, ...profiles.Mutation }
+}
+
+export default resolvers
