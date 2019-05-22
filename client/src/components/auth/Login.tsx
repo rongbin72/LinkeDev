251be86dbb85be2424c87b97eac1e5db5959bee6
Robin <bin.googol@gmail.com>
2019-05-22 17:57:02 -0400

    Immigrate to GraphQL

--- client/src/components/auth/Login.tsx
+++ client/src/components/auth/Login.tsx
@@ -1,64 +1,90 @@
 import React, { useState } from 'react'
+import { Query } from 'react-apollo'
 import { Link, Redirect } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { login } from '../../actions/auth'
-import { LoginProps, StoreState, LoginForm } from '../../../common/types'
+import { LoginForm } from '../../../common/types'
+import { client } from '../../App'
+import {
+  CURRENT_USER,
+  LOGIN,
+  LoginResponse,
+  UserResponse
+} from '../../graphql/queries/authQuery'
+import Loading from '../layout/Loading'
 
-const Login: React.FC<LoginProps> = ({ login, isAuth }) => {
-  const [formData, setFormData] = useState<LoginForm>({ email: '', password: '' })
+const Login: React.FC = () => {
+  const [formData, setFormData] = useState<LoginForm>({
+    email: '',
+    password: ''
+  })
   const { email, password } = formData
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setFormData({ ...formData, [e.target.name]: e.target.value })
 
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const login = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    login(email, password)
+    try {
+      const res = await client.query<LoginResponse>({
+        query: LOGIN,
+        variables: { email, password }
+      })
+      localStorage.setItem('token', res.data.login.token)
+      client.resetStore()
+    } catch (error) {
+      console.error(error.message)
+    }
   }
 
   // Redirect if logged in, else show login page
-  return isAuth ? (
-    <Redirect to='/dashboard' />
-  ) : (
-    <>
-      <section className='container'>
-        <h1 className='large text-primary'>Sign In</h1>
-        <p className='lead'>
-          <i className='fas fa-user' /> Sign into Your Account
-        </p>
-        <form className='form' action='create-profile.html' onSubmit={e => onSubmit(e)}>
-          <div className='form-group'>
-            <input
-              type='email'
-              placeholder='Email Address'
-              name='email'
-              value={email}
-              onChange={e => onChange(e)}
-            />
-          </div>
-          <div className='form-group'>
-            <input
-              type='password'
-              placeholder='Password'
-              name='password'
-              minLength={6}
-              value={password}
-              onChange={e => onChange(e)}
-            />
-          </div>
-          <input type='submit' className='btn btn-primary' value='Login' />
-        </form>
-        <p className='my-1'>
-          Don't have an account? <Link to='/register'>Sign Up</Link>
-        </p>
-      </section>
-    </>
+  return (
+    <Query<UserResponse, null> query={CURRENT_USER}>
+      {({ loading, error, data }) => {
+        if (loading) return <Loading />
+        // auth fail, show login page
+        if (error)
+          return (
+            <section className='container'>
+              <h1 className='large text-primary'>Sign In</h1>
+              <p className='lead'>
+                <i className='fas fa-user' /> Sign into Your Account
+              </p>
+              <form
+                className='form'
+                action='create-profile.html'
+                onSubmit={e => login(e)}>
+                <div className='form-group'>
+                  <input
+                    type='email'
+                    placeholder='Email Address'
+                    name='email'
+                    value={email}
+                    onChange={e => onChange(e)}
+                  />
+                </div>
+                <div className='form-group'>
+                  <input
+                    type='password'
+                    placeholder='Password'
+                    name='password'
+                    minLength={6}
+                    value={password}
+                    onChange={e => onChange(e)}
+                  />
+                </div>
+                <input
+                  type='submit'
+                  className='btn btn-primary'
+                  value='Login'
+                />
+              </form>
+              <p className='my-1'>
+                Don't have an account? <Link to='/register'>Sign Up</Link>
+              </p>
+            </section>
+          )
+        return <Redirect to='/dashboard' />
+      }}
+    </Query>
   )
 }
-const mapStateToProps = (state: StoreState) => ({
-  isAuth: state.auth!.isAuth
-})
-export default connect(
-  mapStateToProps,
-  { login }
-)(Login)
+export default Login
--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -1,47 +1,22 @@
 import React from 'react'
+import { Query } from 'react-apollo'
 import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { NavbarProps, StoreState } from '../../../common/types'
-import { logout } from '../../actions/auth'
+import { CURRENT_USER, UserResponse } from '../../graphql/queries/authQuery'
+import { client } from '../../App'
+import { ApolloQueryResult } from 'apollo-boost'
 
-const Navbar: React.FC<NavbarProps> = ({ isAuth, loading, logout }) => {
-  const authLinks = (
-    <ul>
-      <li>
-        <Link to='/profiles'>
-          <i className='fab fa-connectdevelop' /> <span className='hide-sm'>Developers</span>
-        </Link>
-      </li>
-      <li>
-        <Link to='/posts'>Posts</Link>
-      </li>
-      <li>
-        <Link to='/dashboard'>
-          <i className='fas fa-user' /> <span className='hide-sm'>Dashboard</span>
-        </Link>
-      </li>
-      <li>
-        <Link onClick={logout} to='/'>
-          <i className='fas fa-sign-out-alt' /> <span className='hide-sm'>Logout</span>
-        </Link>
-      </li>
-    </ul>
-  )
-
-  const guestLinks = (
-    <ul>
-      <li>
-        {' '}
-        <Link to='/profiles'>Developers</Link>{' '}
-      </li>
-      <li>
-        <Link to='/register'>Register</Link>
-      </li>
-      <li>
-        <Link to='/login'>Login</Link>
-      </li>
-    </ul>
-  )
+const Navbar: React.FC = () => {
+  const logout = async (
+    refetch: () => Promise<ApolloQueryResult<UserResponse>>
+  ) => {
+    localStorage.removeItem('token')
+    try {
+      await client.clearStore()
+      await refetch()
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
 
   return (
     <nav className='navbar bg-dark'>
@@ -50,17 +25,56 @@ const Navbar: React.FC<NavbarProps> = ({ isAuth, loading, logout }) => {
           <i className='fas fa-code' /> LinkeDev
         </Link>
       </h1>
-      {loading ? null : <>{isAuth ? authLinks : guestLinks}</>}
+      <Query<UserResponse, null> query={CURRENT_USER}>
+        {({ loading, error, data, refetch }) => {
+          // links before login
+          if (error)
+            return (
+              <ul>
+                <li>
+                  {' '}
+                  <Link to='/profiles'>Developers</Link>{' '}
+                </li>
+                <li>
+                  <Link to='/register'>Register</Link>
+                </li>
+                <li>
+                  <Link to='/login'>Login</Link>
+                </li>
+              </ul>
+            )
+
+          if (loading) return null
+          // inks after login
+          return (
+            <ul>
+              <li>
+                <Link to='/profiles'>
+                  <i className='fab fa-connectdevelop' />{' '}
+                  <span className='hide-sm'>Developers</span>
+                </Link>
+              </li>
+              <li>
+                <Link to='/posts'>Posts</Link>
+              </li>
+              <li>
+                <Link to='/dashboard'>
+                  <i className='fas fa-user' />{' '}
+                  <span className='hide-sm'>Dashboard</span>
+                </Link>
+              </li>
+              <li>
+                <Link onClick={e => logout(refetch)} to='/'>
+                  <i className='fas fa-sign-out-alt' />{' '}
+                  <span className='hide-sm'>Logout</span>
+                </Link>
+              </li>
+            </ul>
+          )
+        }}
+      </Query>
     </nav>
   )
 }
 
-const mapStateToProps = (state: StoreState) => ({
-  isAuth: state.auth!.isAuth,
-  loading: state.auth!.loading
-})
-
-export default connect(
-  mapStateToProps,
-  { logout }
-)(Navbar)
+export default Navbar
