e3d37dbe3ab8051ca307dd8ec3265143fc32f392
Robin <bin.googol@gmail.com>
2019-05-22 17:54:37 -0400

    Immigrate to GraphQL For debuging purpose, keep redux provider for now

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,44 @@
+import ApolloClient from 'apollo-boost'
 import React, { useEffect } from 'react'
+import { ApolloProvider } from 'react-apollo'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { loadUser } from './actions/auth'
 import './App.css'
 import Landing from './components/layout/Landing'
 import Navbar from './components/layout/Navbar'
 import Routes from './components/routing/Routes'
+import { CURRENT_USER } from './graphql/queries/authQuery'
 import store from './store'
-import setAuthToken from './utils/setAuthToken'
 
-if (localStorage.token) setAuthToken(localStorage.token)
+export const client = new ApolloClient({
+  uri: '/graphql',
+  request: async op => {
+    op.setContext({
+      headers: { 'x-auth-token': localStorage.getItem('token') }
+    })
+  }
+})
 
 const App: React.FC = () => {
   useEffect(() => {
-    store.dispatch(loadUser())
+    client.query({ query: CURRENT_USER }).catch((error: any) => {
+      console.error(error.message)
+    })
   }, [])
 
   return (
     <Provider store={store}>
-      <Router>
-        <>
-          <Navbar />
-          <Switch>
-            <Route exact path='/' component={Landing} />
-            <Route component={Routes} />
-          </Switch>
-        </>
-      </Router>
+      <ApolloProvider client={client}>
+        <Router>
+          <>
+            <Navbar />
+            <Switch>
+              <Route exact path='/' component={Landing} />
+              <Route component={Routes} />
+            </Switch>
+          </>
+        </Router>
+      </ApolloProvider>
     </Provider>
   )
 }
