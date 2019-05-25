0033e2bd52056c55ace6836d5950ebd7e28ee8aa
Robin <bin.googol@gmail.com>
2019-05-25 19:25:55 -0400

    Add profile queries

--- client/src/graphql/gql/profile.ts
+++ client/src/graphql/gql/profile.ts
@@ -3,9 +3,19 @@ import { gql } from 'apollo-boost'
 export const MY_PROFILE = gql`
   query MyProfile {
     myProfile {
-      _id
-      user {
-        name
+      experience {
+        _id
+        company
+        title
+        from
+        to
+      }
+      education {
+        _id
+        school
+        degree
+        from
+        to
       }
     }
   }
@@ -15,6 +25,34 @@ export const PROFILES = gql`
   query Profiles {
     profiles {
       _id
+    }
+  }
+`
+
+export const DELETE_EXPERIENCE = gql`
+  mutation DeleteExperience($id: ID!) {
+    deleteExperience(id: $id) {
+      experience {
+        _id
+      }
+    }
+  }
+`
+
+export const DELETE_EDUCATION = gql`
+  mutation DeleteEducation($id: ID!) {
+    deleteEducation(id: $id) {
+      education {
+        _id
+      }
+    }
+  }
+`
+
+export const DELETE_ACCOUNT = gql`
+  mutation DeleteAccount {
+    deleteAccount {
+      msg
     }
   }
 `
