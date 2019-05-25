1002edec1e01846b52ed9f1335e6c6c949363c7f
Robin <bin.googol@gmail.com>
2019-05-25 19:26:35 -0400

    show nothing while loading

--- client/src/components/routing/PrivateRoute.tsx
+++ client/src/components/routing/PrivateRoute.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { useQuery } from 'react-apollo-hooks';
-import { Redirect, Route } from 'react-router';
-import { PrivateRouteProps } from '../../../common/types';
-import { AUTH_STATUS } from '../../graphql/gql/auth';
-import { AuthStatus } from '../../graphql/types';
+import React from 'react'
+import { useQuery } from 'react-apollo-hooks'
+import { Redirect, Route } from 'react-router'
+import { PrivateRouteProps } from '../../../common/types'
+import { AUTH_STATUS } from '../../graphql/gql/auth'
+import { AuthStatus } from '../../graphql/types'
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({
   component: PrivateComponent,
   ...rest
 }) => {
-  const { data: auth } = useQuery<AuthStatus, null>(AUTH_STATUS)
+  const { loading, data: auth } = useQuery<AuthStatus, null>(AUTH_STATUS)
 
-  return auth && auth.authStatus && auth.authStatus.isAuth ? (
+  if (loading || !auth || !auth.authStatus) return <></>
+
+  return auth.authStatus.isAuth ? (
     <Route {...rest} render={props => <PrivateComponent {...props} />} />
   ) : (
     <Route {...rest} render={_ => <Redirect to='/login' />} />
