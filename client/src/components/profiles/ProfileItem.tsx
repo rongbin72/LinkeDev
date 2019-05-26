0331c41ac51df8b3b2dbb88339320d614b21f305
Robin <bin.googol@gmail.com>
2019-05-26 14:46:18 -0400

    Remove view profile link Instead the whole card becomes a line

--- client/src/components/profiles/ProfileItem.tsx
+++ client/src/components/profiles/ProfileItem.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { ProfileItemProps } from '../../../common/types'
+import { Profiles_profiles } from '../../graphql/types'
+
+interface ProfileItemProps {
+  profile: Profiles_profiles
+}
 
 const ProfileItem: React.FC<ProfileItemProps> = ({ profile }) => {
   const {
@@ -12,27 +16,26 @@ const ProfileItem: React.FC<ProfileItemProps> = ({ profile }) => {
   } = profile
 
   return (
-    <div className='profile bg-light card'>
-      <img src={avatar} alt='' className='round-img' />
-      <div>
-        <h2>{name}</h2>
-        <p>
-          {status} {company && <span> @ {company}</span>}
-        </p>
-        <p className='my-1'>{location && <span>{location}</span>}</p>
-        <Link to={`/profile/${_id}`} className='btn btn-primary'>
-          View Profile
-        </Link>
+    <Link to={`/profile/${_id}`}>
+      <div className='profile bg-light card'>
+        <img src={avatar} alt='' className='round-img' />
+        <div>
+          <h2>{name}</h2>
+          <p>
+            {status} {company && <span> @ {company}</span>}
+          </p>
+          <p className='my-1'>{location && <span>{location}</span>}</p>
+        </div>
+        <ul>
+          {skills &&
+            skills.slice(0, 4).map((skill, index) => (
+              <li key={index} className='text-primary'>
+                <i className='fas fa-check' /> {skill}
+              </li>
+            ))}
+        </ul>
       </div>
-      <ul>
-        {skills &&
-          skills.slice(0, 4).map((skill, index) => (
-            <li key={index} className='text-primary'>
-              <i className='fas fa-check' /> {skill}
-            </li>
-          ))}
-      </ul>
-    </div>
+    </Link>
   )
 }
 
