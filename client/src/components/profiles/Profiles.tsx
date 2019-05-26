bc38dc2646c2bed61c748476e44e2ba1d394f119
Robin <bin.googol@gmail.com>
2019-05-26 14:45:24 -0400

    Migrate to GraphQL

--- client/src/components/profiles/Profiles.tsx
+++ client/src/components/profiles/Profiles.tsx
@@ -1,44 +1,43 @@
-import React, { useEffect } from 'react'
-import { ProfilesProps, StoreState } from '../../../common/types'
-import { connect } from 'react-redux'
-import { getProfiles } from '../../actions/profile'
-import Spinner from '../layout/Spinner'
+import React from 'react'
+import { useQuery } from 'react-apollo-hooks'
+import { toast } from 'react-toastify'
+import { PROFILES } from '../../graphql/gql/profile'
+import { Profiles as TProfiles } from '../../graphql/types'
+import showAlert from '../../utils/showAlert'
+import Loading from '../layout/Loading'
 import ProfileItem from './ProfileItem'
 
-const Profiles: React.FC<ProfilesProps> = ({ getProfiles, profiles, loading }) => {
-  useEffect(() => {
-    getProfiles()
-  }, [getProfiles])
+const Profiles: React.FC = () => {
+  const { error, loading, data: profiles } = useQuery<TProfiles>(PROFILES)
 
-  return (
+  if (error) {
+    showAlert('Something went wrong', toast.TYPE.ERROR)
+    console.error(error)
+    return null
+  }
+
+  return loading ? (
+    <Loading />
+  ) : (
     <>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <>
-          <h1 className='large text-primary'>Developers</h1>
-          <p className='lead'>
-            <i className='fab fa-connectdevelop'> Browse and connect with developers</i>
-          </p>
-          <div className='profiles'>
-            {profiles && profiles.length > 0 ? (
-              profiles.map(profile => <ProfileItem key={profile._id} profile={profile} />)
-            ) : (
-              <h4>No Profile Found ...</h4>
-            )}
-          </div>
-        </>
-      )}
+      <h1 className='large text-primary'>Developers</h1>
+      <p className='lead'>
+        <i className='fab fa-connectdevelop'>
+          {' '}
+          Browse and connect with developers
+        </i>
+      </p>
+      <div className='profiles'>
+        {profiles && profiles.profiles.length > 0 ? (
+          profiles.profiles.map(profile => (
+            <ProfileItem key={profile._id} profile={profile} />
+          ))
+        ) : (
+          <h4>No Profile Found ...</h4>
+        )}
+      </div>
     </>
   )
 }
 
-const mapStateToProps = (state: StoreState) => ({
-  profiles: state.profile!.profiles,
-  loading: state.profile!.loading
-})
-
-export default connect(
-  mapStateToProps,
-  { getProfiles }
-)(Profiles)
+export default Profiles
