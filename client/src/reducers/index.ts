f17d55e0a39c10d05913880ad883feece71726de
Robin <bin.googol@gmail.com>
2019-05-27 23:21:44 -0400

    delete redux code

--- client/src/reducers/index.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import auth from './auth'
-import profile from './profile'
-import post from './post'
-
-export default combineReducers({ auth, profile, post })
